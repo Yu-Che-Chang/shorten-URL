9ba6ef15e94a5406e9ea28ddcf04d1856d20f8f6
Yu-Che-Chang <a79967167@gmail.com>
2023-01-11 01:08:02 +0800

    feat: 設置 handlebars, main/index page

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+'use strict';
 const port = 3000
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
+
+// set template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+// app.use('/images',express.static('images'))
 
 require('./config/mongoose')
 
 app.get('/', (req, res) => {
-  res.send('good job!')
+  res.render('index')
 })
 
 app.listen(port, () => {
