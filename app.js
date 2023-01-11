95c8d5070d6dc6b5ab4a403be671e8a71f6af784
Yu-Che-Chang <a79967167@gmail.com>
2023-01-11 13:11:28 +0800

    feat: 修正 index page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-// app.use('/images',express.static('images'))
+// app.use('/images', express.static('images'))
 
 require('./config/mongoose')
 
